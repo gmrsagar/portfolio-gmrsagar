ac5de9ec0b27c0d7a75aaac9b9e6cce9ca1e7b79
Akriti Bhusal <akriti.ab@gmail.com>
2018-12-17 08:00:44 +1100

    Add global color settings to settings.sass

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        Test
+        <div class="row">
+            
+        </div>
       </div>
     );
   }
