ffeab55e998169e9de1f85a4c7a698a05bf871dd
Akriti Bhusal <akriti.ab@gmail.com>
2018-12-30 17:41:13 +1100

    Fix project card overflow

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -15,21 +15,27 @@ class Work extends Component {
 
 	render() {
 		return(
-			<div className="main-section">
-				<h1>My Works</h1>
-				<p className="description-text">
-						Some of the projects that I work on when I am  <br/> not working on the rest of my projects
-				</p>
-				<div className="project-card--holder mt-4">
-					<div className="row no-gutters">
-						<div className="col-11">
-							<div className="row">
-								{ this.state.works.map( work => <div className="col-md-6"><ProjectCard work={work}/></div> )}
+			<div className="row">
+				<div className="offset-2"></div>
+				<div className="col-10">
+					<div className="main-section">
+						<h1>My Works</h1>
+						<p className="description-text">
+								Some of the projects that I work on when I am  <br/> not working on the rest of my projects
+						</p>
+						<div className="project-card--holder mt-4">
+							<div className="row no-gutters">
+								<div className="col-11">
+									<div className="row">
+										{ this.state.works.map( work => <div className="col-md-6"><ProjectCard work={work}/></div> )}
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			</div>	
+
 		);
 	}
 }
