986c570da545b2bcc1a350299032156ca4b23db5
Sagar Ghimire <gmrsagar@gmail.com>
2019-01-16 17:31:46 +1100

    Updates

---
+++ precache-manifest.48e8ff41b9f44474eccd0b21be2b29e5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e5092a2fe331cb2dbcf5de719a81f6d",
+    "url": "/portfolio-gmrsagar/static/media/twitter.9e5092a2.svg"
+  },
+  {
+    "revision": "4fc9e75aaa3a80e2a7f3",
+    "url": "/portfolio-gmrsagar/static/css/main.1487753d.chunk.css"
+  },
+  {
+    "revision": "93e80deaec29d9d4906ad8da3757a561",
+    "url": "/portfolio-gmrsagar/static/media/hero-illustration2.93e80dea.svg"
+  },
+  {
+    "revision": "7779a334e5effd7732bb",
+    "url": "/portfolio-gmrsagar/static/js/1.7779a334.chunk.js"
+  },
+  {
+    "revision": "0523f69121f4096495cf",
+    "url": "/portfolio-gmrsagar/static/js/runtime~main.0523f691.js"
+  },
+  {
+    "revision": "3b65622ca12aef3f348ccd9bf84ed02c",
+    "url": "/portfolio-gmrsagar/static/media/home.3b65622c.svg"
+  },
+  {
+    "revision": "a7b677b832df52ef66d55a25cd8ba722",
+    "url": "/portfolio-gmrsagar/static/media/work.a7b677b8.svg"
+  },
+  {
+    "revision": "1dde72ed09bdda9e83a2483f90f2d64e",
+    "url": "/portfolio-gmrsagar/static/media/about.1dde72ed.svg"
+  },
+  {
+    "revision": "91c70440a23de3a8f8aec921bbf96c24",
+    "url": "/portfolio-gmrsagar/static/media/contact.91c70440.svg"
+  },
+  {
+    "revision": "9fbf7fb4990d3c2f30fbf3c40938eb0b",
+    "url": "/portfolio-gmrsagar/static/media/git.9fbf7fb4.svg"
+  },
+  {
+    "revision": "533b69eb1e0e2c0d189872aae82eefbd",
+    "url": "/portfolio-gmrsagar/static/media/linkedin.533b69eb.svg"
+  },
+  {
+    "revision": "4fc9e75aaa3a80e2a7f3",
+    "url": "/portfolio-gmrsagar/static/js/main.4fc9e75a.chunk.js"
+  },
+  {
+    "revision": "7b2fb7c3d07cc5be2df5198740034421",
+    "url": "/portfolio-gmrsagar/static/media/home_active.7b2fb7c3.svg"
+  },
+  {
+    "revision": "42b0944255132e8ea9e03f7feab8e539",
+    "url": "/portfolio-gmrsagar/static/media/work_active.42b09442.svg"
+  },
+  {
+    "revision": "c4b2bd6ad371606396d0ad9d201469f4",
+    "url": "/portfolio-gmrsagar/static/media/about_active.c4b2bd6a.svg"
+  },
+  {
+    "revision": "18f245dc2878f27e204a2714df8e7170",
+    "url": "/portfolio-gmrsagar/static/media/contact_active.18f245dc.svg"
+  },
+  {
+    "revision": "9a622255ff05c78758fbb1905a7e9258",
+    "url": "/portfolio-gmrsagar/static/media/git_active.9a622255.svg"
+  },
+  {
+    "revision": "a85f8ccf3c7e87f40580badd6469c120",
+    "url": "/portfolio-gmrsagar/static/media/linkedin_active.a85f8ccf.svg"
+  },
+  {
+    "revision": "886804232b345e65110dc3d5f38f7d59",
+    "url": "/portfolio-gmrsagar/static/media/twitter_active.88680423.svg"
+  },
+  {
+    "revision": "c256499796a4465f8b046a6eb3d5cb9f",
+    "url": "/portfolio-gmrsagar/static/media/hero-illustration.c2564997.svg"
+  },
+  {
+    "revision": "e9fde07cb1ff39826459ec6945ee737b",
+    "url": "/portfolio-gmrsagar/static/media/hero-illustration3.e9fde07c.svg"
+  },
+  {
+    "revision": "7779a334e5effd7732bb",
+    "url": "/portfolio-gmrsagar/static/css/1.2b389cbb.chunk.css"
+  },
+  {
+    "revision": "a0e4ba974005a9c9adf2f77bc0d1b6cc",
+    "url": "/portfolio-gmrsagar/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/portfolio-gmrsagar/precache-manifest.48e8ff41b9f44474eccd0b21be2b29e5.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/portfolio-gmrsagar/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
